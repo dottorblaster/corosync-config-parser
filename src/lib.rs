584e771348b6e9a0323f2b9efb6f4b05c04215c4
Alessio Biancalana <dottorblaster@gmail.com>
2022-06-11 11:00:11 +0200

    Remove println!s and refactor parse function

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,10 @@ pub use config::ConfigBlock;
 pub use error::{Error as ParseError, Result};
 
 pub fn parse(data: String) -> Result<ConfigBlock> {
-    let chars = Box::leak(data.into_boxed_str());
-    let corosync_tokens = lexer::run(Box::new(chars.chars())).unwrap();
-    println!("{:?}", corosync_tokens);
+    let owned_string = Box::leak(data.into_boxed_str());
 
-    let corosync_conf = parser::run(Box::new(corosync_tokens.into_iter()));
-    println!("{:?}", corosync_conf);
-    corosync_conf
+    match lexer::run(Box::new(owned_string.chars())) {
+        Ok(tokens) => parser::run(Box::new(tokens.into_iter())),
+        Err(err) => Err(err),
+    }
 }
